01516aefa9f99e87e6229ca318c61b9fc47dd5af
Ronanlock <robertsregime@gmail.com>
2020-07-20 18:30:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random 
+
+#create list 10
+#create list 100
+#create list 100000
+
+#For loop for list 10
+#add rand num
+#For loop for list 100
+#add rand num 
+#for list 100000
+#add rand num
